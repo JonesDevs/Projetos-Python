d5189844137da31761a25de006d55dc6896fe0e1
João Pedro <160535586+JonesDevs@users.noreply.github.com>
2026-09-10 13:32:33 -0300

    Arquivo Python incompleto
    Arrumando / Aprendendo

---
+++ Arquivos/Contador.py
@@ -0,0 +1,8 @@
+while True:
+    print("Digite o primeiro número: ")
+    n1 = int(input())
+    print("Digite o segundo número: ")
+    n2 = int(input())
+    for i in range(n1, n2):
+        print(i)
+        break
